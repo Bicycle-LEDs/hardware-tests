c67f632869ceb9a055184e5eb9b0dc430ffb0563
Bajojajo-xD <81306360+Bajojajo-xD@users.noreply.github.com>
2022-11-29 19:57:36 +0100

    ⚙️ Add button test

---
+++ testers/button/button.ino
@@ -0,0 +1,19 @@
+int button = 6, buttonLastState=0,
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  pinMode(button, INPUT_PULLUP);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  if(digitalRead(button) == 1 && buttonLastState == 0) {
+    buttonLastState=1;
+    Serial.println("Button active");
+  } else buttonLastState=0;
+  
+}
